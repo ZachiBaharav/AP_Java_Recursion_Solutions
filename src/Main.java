922e54ecd66388f57f13a5571ecc43847daddcce
ZachiBaharav <zachi@baharav.org>
2017-11-24 21:51:25 -0800

    Tower of Hanoi added.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
 
-/**
- *
- * @author Dr. Baharav
- */
+
+
+
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println("Testing recursion functions.");
+        
+        RecursionSolver r = new RecursionSolver();
+        
+        int n = 8;
+        System.out.println("Factorial: " + n + "! = " + r.factorial(n));
+        
+        String str = "AbbA";
+        System.out.println("Palindrome: " + str + " is palindrome: " + r.isPalindrome(str));
+        str = "AbcdbA";
+        System.out.println("Palindrome: " + str + " is palindrome: " + r.isPalindrome(str));
+        
+        
+        // Towers of Hanoi
+        // https://www.mathsisfun.com/games/towerofhanoi.html
+        int N=3;
+        r.towerOfHanoi(N,"A","C","B");
+     
+        
+    }
+    
 }
--- src/RecursionSolver.java
+++ src/RecursionSolver.java
@@ -5,4 +5,32 @@
  */
 public class RecursionSolver {
 
+    public int factorial(int n) {
+        if (n<=0) return 0;
+        if (n==1) return 1;
+        return n*factorial(n-1);
+    }
+    
+    public boolean isPalindrome(String str) {
+        if (str.length() <=1) return true;
+        if (str.charAt(0) != str.charAt(str.length()-1)) return false;
+        return isPalindrome(str.substring(1,str.length()-1));
+    }
+    
+    
+    public void towerOfHanoi(int n, String from, String to, String aux) {
+        if (n==1)
+            System.out.println("Move " + from + " ==> " + to);
+        else {
+            //move n-1 disks 'from' ==> 'aux'
+            towerOfHanoi(n-1,from,aux,to);
+            //move 1 disk    'from' ==> 'to'
+            System.out.println("Move " + from + " ==> " + to);
+            //move n-1 disks  'aux' ==> 'to'
+            towerOfHanoi(n-1,aux,to,from);
+        }
+
+
+    }
+
 }
